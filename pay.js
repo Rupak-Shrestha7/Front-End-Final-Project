b5c30155d6dadb6b1727b8f8092fec668e247f4e
JS <rupakshrestha1111@gmail.com>
2024-04-20 03:26:40 +0545

    Delete

--- pay.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("paymentForm").addEventListener("submit", function(event){
-  event.preventDefault();
-  setTimeout(() => {
-      alert("Payment processed!, Continue more shopping....");
-      window.location.href = 'shop.html';
-  }, 2000);
-});
